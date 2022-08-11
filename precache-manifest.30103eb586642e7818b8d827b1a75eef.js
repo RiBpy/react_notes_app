633ca58e73dffce15825c03d855524cff320b6ad
RiBpy <77004199+RiBpy@users.noreply.github.com>
2022-08-12 01:28:42 +0600

    Updates

---
+++ precache-manifest.30103eb586642e7818b8d827b1a75eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8344825ce6d255ebdad71452004af037",
+    "url": "/react_notes_app/index.html"
+  },
+  {
+    "revision": "de46636cd794a4417a42",
+    "url": "/react_notes_app/static/css/main.7b339a6c.chunk.css"
+  },
+  {
+    "revision": "868e51aa4835b278d5a8",
+    "url": "/react_notes_app/static/js/2.d0c1d5aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_notes_app/static/js/2.d0c1d5aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de46636cd794a4417a42",
+    "url": "/react_notes_app/static/js/main.39bd52cd.chunk.js"
+  },
+  {
+    "revision": "51011a88fcb1d9dd2efc",
+    "url": "/react_notes_app/static/js/runtime-main.cac1ffa3.js"
+  }
+]);
